b8f8a34ffb9ed2db054ec8592a2f81ae4bfbc16d
Bojie Jiang <jiangbojie@gmail.com>
2015-10-30 10:16:06 -0700

    new basic map

--- app/src/main/java/com/bojie/map_basic_android/MainActivity.java
+++ app/src/main/java/com/bojie/map_basic_android/MainActivity.java
@@ -2,67 +2,33 @@ package com.bojie.map_basic_android;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity {
-
-    GoogleMap mGoogleMap;
-    private static LatLng MountainView = new LatLng(37.42, -122.08);
-
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Initial Map
-        try {
-
-            if (mGoogleMap == null) {
-                mGoogleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        mGoogleMap.getUiSettings().setZoomGesturesEnabled(true);
-
-        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(MountainView, 15));
-
-        Marker marker = mGoogleMap.addMarker(new MarkerOptions()
-                .position(MountainView)
-                .title("Have a nice day!"));
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+//        map.setPadding(0,0,0,100);
+//        LatLngBounds bounds = map.getProjection().getVisibleRegion().latLngBounds;
+//        Log.d("bounds", bounds.toString());
     }
 }
