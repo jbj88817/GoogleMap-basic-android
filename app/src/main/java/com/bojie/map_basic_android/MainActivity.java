a7d78d4e47d763d60fa86364fed308ed22f3374d
Bojie Jiang <jiangbojie@gmail.com>
2015-11-02 11:15:38 -0800

    update buildTools

--- app/src/main/java/com/bojie/map_basic_android/MainActivity.java
+++ app/src/main/java/com/bojie/map_basic_android/MainActivity.java
@@ -1,7 +1,7 @@
 package com.bojie.map_basic_android;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
